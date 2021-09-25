aa6ec4c7d50352afbb509c7dfd0357c9b1e0ea24
MolinoSimone <s.molino.1767@vallauri.edu>
2021-09-25 09:09:01 +0200

    First commit

---
+++ Esercizi/WindowsForms01/WindowsForms01/Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms01
+{
+    public partial class Form2 : Form
+    {
+        private int n;
+        private TextBox txt;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form2(int n)
+        {
+            InitializeComponent();
+            this.n = n;
+        }
+
+        public Form2(TextBox txt)
+        {
+            InitializeComponent();
+            this.txt = txt;
+           // txt.Text="Ciao da f2";
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnF2Leggi_Click(object sender, EventArgs e)
+        {
+            
+            if (txt.Text==null)
+            {
+                MessageBox.Show("Non è stato passato l'oggetto del textBox");
+            }
+            else
+                MessageBox.Show(txt.Text);
+        }
+
+        private void btnInviaAForm1_Click(object sender, EventArgs e)
+        {
+            if (txt.Text == null)
+            {
+                MessageBox.Show("Non è stato passato l'oggetto del textBox");
+            }
+            else
+                txt.Text = txtF2.Text;
+        }
+
+        private void btnF2ApriFiglia_Click(object sender, EventArgs e)
+        {
+            FormFiglia f = new FormFiglia();
+            this.AddOwnedForm(f);
+            f.Show();
+        }
+    }
+}
---
+++ Esercizi/WindowsForms01/WindowsForms01/FormFiglia.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace WindowsForms01
+{
+    partial class FormFiglia
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDiChiSonoFiglia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDiChiSonoFiglia
+            // 
+            this.btnDiChiSonoFiglia.Location = new System.Drawing.Point(13, 13);
+            this.btnDiChiSonoFiglia.Name = "btnDiChiSonoFiglia";
+            this.btnDiChiSonoFiglia.Size = new System.Drawing.Size(210, 68);
+            this.btnDiChiSonoFiglia.TabIndex = 0;
+            this.btnDiChiSonoFiglia.Text = "Di Chi Sono Figlia?";
+            this.btnDiChiSonoFiglia.UseVisualStyleBackColor = true;
+            this.btnDiChiSonoFiglia.Click += new System.EventHandler(this.btnDiChiSonoFiglia_Click);
+            // 
+            // FormFiglia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDiChiSonoFiglia);
+            this.Name = "FormFiglia";
+            this.Text = "FormFiglia";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDiChiSonoFiglia;
+    }
+}
